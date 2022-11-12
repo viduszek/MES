824d7d2b445d8b412067f450ebb822b0c758fb64
viduszek <widuch2001@gmail.com>
2022-11-12 20:08:36 +0100

    Text file added, most inputs ready

--- main.py
+++ main.py
@@ -1,28 +1,79 @@
-import numpy
+import numpy as np
 
 
 class Node(object):
-    x = float(0)
-    y = float(0)
-    temp = float(0)
-    WB = bool(False)
+    x = float()
+    y = float()
+    WB = bool()
 
 
 class Element(object):
-    ID = []
+    ID = [4]
 
 
 class GlobalData(object):
-    simTime = int(0)
-    simStepTime = int(0)
-    conductivity = int(0)
-    alpha = int(0)
-    tot = int(0)
-    initialTemperature = int(0)
-    density = int(0)
-    specificHeat = int(0)
+
+    def getdata(filename):
+        with open(filename, 'r') as f:
+            simTime = int(f.readline().split()[-1])
+            simStepTime = int(f.readline().split()[-1])
+            conductivity = int(f.readline().split()[-1])
+            alpha = int(f.readline().split()[-1])
+            tot = int(f.readline().split()[-1])
+            initialTemperature = int(f.readline().split()[-1])
+            density = int(f.readline().split()[-1])
+            specificHeat = int(f.readline().split()[-1])
+            # print(simTime, simStepTime, conductivity, alpha, tot, initialTemperature, density, specificHeat)
 
 
 class Grid(object):
-    nodesNumber = int(0)
-    elementsNumber = int(0)
+    nodesNumber = int()
+    elementsNumber = int()
+
+    def getdata(filename):
+        with open(filename, 'r') as f:
+            for i in range(8):          # skip first data sets
+                trash = f.readline()
+            nodesNumber = int(f.readline().split()[-1])
+            elementsNumber = int(f.readline().split()[-1])
+
+            trash = f.readline()
+
+            nodes = []
+            for i in range(nodesNumber):
+                temp = f.readline().replace(",", "").strip().split()
+                nodes.append(temp)
+            print(nodes)
+
+            trash = f.readline()
+
+            elements = []
+            for i in range(elementsNumber):
+                temp = f.readline().replace(",", "").strip().split()
+                elements.append(temp)
+            # print(elements)
+
+
+class SC(object):
+    intPt2 = [-1/np.sqrt(3), 1/np.sqrt(3)]  # integral points
+    ptWeight2 = [1, 1]
+
+    intPt3 = [-np.sqrt(0.6), 0, np.sqrt(0.6)]  # integral points (#3)
+    ptWeight3 = [0.555555, 0.888888, 0.555555]
+
+
+class Element4(object):
+    ksi = []
+    eta = []
+
+
+# Choose input file:
+filename = "Test1_4_4.txt"
+# filename = "Test2_4_4_MixGrid.txt"
+# filename = "Test3_31_31_kwadrat.txt"
+
+# Import data from the file.
+GlobalData.getdata(filename)
+Grid.getdata(filename)
+
+#
