430c128976aa0543f09d21837e7a879d39d54b44
viduszek <widuch2001@gmail.com>
2022-11-16 16:18:51 +0100

    Data and data import is ready.

--- main.py
+++ main.py
@@ -1,91 +1,153 @@
+# =============================== Libraries/Packages
 import numpy as np
+import scipy as sp
+
+# =============================== Classes
 
 
-class Node(object):
+class Node:
     x = float()
     y = float()
     BC = bool()
 
-
-class Element(object):
-    ID = [4]
-
-
-class GlobalData(object):
-    global simTime
-    global simStepTime
-    global conductivity
-    global alpha
-    global tot
-    global initialTemperature
-    global density
-    global specificHeat
+    def __init__(self):
+        print("class: Node initialized.")
 
 
-    def getdata(filename):
-        with open(filename, 'r') as f:
-            simTime = f.readline().split()[-1]
-            simStepTime = f.readline().split()[-1]
-            conductivity = f.readline().split()[-1]
-            alpha = f.readline().split()[-1]
-            tot = f.readline().split()[-1]
-            initialTemperature = f.readline().split()[-1]
-            density = f.readline().split()[-1]
-            specificHeat = f.readline().split()[-1]
-            print(simTime, simStepTime, conductivity, alpha, tot, initialTemperature, density, specificHeat)
-            return simTime, simStepTime, conductivity, alpha, tot, initialTemperature, density, specificHeat
+class Element:
+    ID = []  # up to 4!
+    # ID = [4]
+
+    def __init__(self):
+        print("class: Element initialized.")
 
 
-class Grid(object):
+class GlobalData:  # there's actually no need to declare those variables, but you can do it tho
+    simTime = int()
+    simStepTime = int()
+    conductivity = int()
+    alpha = int()
+    tot = int()
+    initialTemp = int()
+    density = int()
+    specificHeat = int()
+
+    def __init__(self):
+        print("class: GlobalData initialized.")
+
+
+class Grid:
     nodesNumber = int()
     elementsNumber = int()
     nodes = []
     elements = []
 
-    def getdata(filename):
-        with open(filename) as f:
-            for i in range(8):          # skip first data sets, which are imported already
-                trash = f.readline()
-            nodesNumber = int(f.readline().split()[-1])
-            elementsNumber = int(f.readline().split()[-1])
-
-            trash = f.readline()
-
-            for i in range(nodesNumber):
-                temp = f.readline().replace(",", "").strip().split()
-                temp.pop(0)  # removing line number
-                Grid.nodes.append(temp)
-                np.array(Grid.nodes, dtype=float)
-            print(Grid.nodes)
-
-            trash = f.readline()
-
-            for i in range(elementsNumber):
-                temp = f.readline().replace(",", "").strip().split()
-                temp.pop(0)  # removing line number
-                Grid.elements.append(temp)
-                np.array(Grid.elements, dtype=float)
-            print(Grid.elements)
+    def __init__(self):
+        print("class: Grid initialized.")
 
 
 class SC(object):
-    intPt2 = [-1/np.sqrt(3), 1/np.sqrt(3)]  # integral points
+    intPt2 = [-1 / np.sqrt(3), 1 / np.sqrt(3)]  # integral points
     ptWeight2 = [1, 1]
 
     intPt3 = [-np.sqrt(0.6), 0, np.sqrt(0.6)]  # integral points (#3)
     ptWeight3 = [0.555555, 0.888888, 0.555555]
+
+    def __init__(self):
+        print("class: SC initialized.")
 
 
 class Element4(object):
     ksi = []
     eta = []
 
+    def __init__(self):
+        print("class: Element4 initizalized")
 
-# Choose input file:
+
+# =============================== Input Functions
+def getGlobalData(input_file):
+    with open(input_file, 'r') as f:
+        GlobalData.simTime = int(f.readline().split()[-1])
+        GlobalData.simStepTime = int(f.readline().split()[-1])
+        GlobalData.conductivity = int(f.readline().split()[-1])
+        GlobalData.alpha = int(f.readline().split()[-1])
+        GlobalData.tot = int(f.readline().split()[-1])
+        GlobalData.initialTemp = int(f.readline().split()[-1])
+        GlobalData.density = int(f.readline().split()[-1])
+        GlobalData.specificHeat = int(f.readline().split()[-1])
+
+    print(GlobalData.simTime, GlobalData.simStepTime, GlobalData.conductivity, GlobalData.alpha,
+          GlobalData.tot, GlobalData.initialTemp, GlobalData.density, GlobalData.specificHeat)
+    return GlobalData.simTime, GlobalData.simStepTime, GlobalData.conductivity, GlobalData.alpha, \
+        GlobalData.tot, GlobalData.initialTemp, GlobalData.density, GlobalData.specificHeat
+
+
+def getGridData(input_file):
+    with open(input_file) as f:
+        for i in range(8):  # skip first data sets, which are imported already
+            f.readline()
+        nodesNumber = int(f.readline().split()[-1])
+        elementsNumber = int(f.readline().split()[-1])
+
+        f.readline()
+
+        for i in range(nodesNumber):
+            temp = f.readline().replace(",", "").strip().split()
+            temp.pop(0)  # removing line number
+            temp = [float(temp) for temp in temp]  # let's make them float
+            Grid.nodes.append(temp)
+        print(Grid.nodes)
+
+        f.readline()
+
+        for i in range(elementsNumber):
+            temp = f.readline().replace(",", "").strip().split()
+            temp.pop(0)
+            temp = [float(temp) for temp in temp]
+            Grid.elements.append(temp)
+        print(Grid.elements)
+
+        return Grid.nodes, Grid.elements
+
+
+# =============================== Solving Functions
+
+
+
+
+# =============================== To be solved
+
+def f1(x):
+    return 2 * x**2 + 3 * x - 8
+
+
+def f2(ksi, eta):
+    return -5 * ksi**2 * eta + 2 * ksi * eta**2 + 10
+
+
+def f3(x):
+    return 3 * x**2 - 6 * x + 1
+
+
+
+# =============================== Main
+
+# Choose input file.
 filename = "Test1_4_4.txt"
 # filename = "Test2_4_4_MixGrid.txt"
 # filename = "Test3_31_31_kwadrat.txt"
 
+# Class initialization
+a = GlobalData()
+b = Grid()
+c = SC()
+d = Node()
+e = Element()
+f = Element4()
+
 # Import data from the file.
-GlobalData.getdata(filename)
-Grid.getdata(filename)
+getGlobalData(filename)
+getGridData(filename)
+
+
